4679c195e3f979da10f693a9873803b7fce4b5c2
Jordan Evens <jordan.evens@canada.ca>
2021-11-19 14:40:19 -0500

    changed to use function lookup table in DistanceAtTime()

--- cffdrs.core/R/DistanceAtTime.r
+++ cffdrs.core/R/DistanceAtTime.r
@@ -1,3 +1,27 @@
+.DistanceAtTimeFunctions <- (function()
+{
+  a1 <- function(CFB)
+  {
+    #Eq. 72 (FCFDG 1992)
+    #Calculate the alpha constant for the DISTt calculation
+    alpha <- 0.115
+    return (alpha)
+  }
+  a2 <- function(CFB)
+  {
+    #Eq. 72 (FCFDG 1992)
+    #Calculate the alpha constant for the DISTt calculation
+    alpha <- 0.115 - 18.8 * (CFB**2.5) * exp(-8* CFB)
+    return (alpha)
+  }
+  result <- list(a1, a2, a2, a2, a2, a2, a2,
+                 a1, a2, a2, a2, a2, a1, a1,
+                 a1, a1, a1)
+  names(result) <- c("C1", "C2", "C3", "C4", "C5", "C6", "C7",
+                     "D1", "M1", "M2", "M3", "M4", "O1A", "O1B",
+                     "S1", "S2", "S3")
+  return (result)
+})()
 #' Distance at time t calculator
 #' 
 #' Calculate the Head fire spread distance at time t. In the documentation this 
@@ -19,9 +43,7 @@ DistanceAtTime <- function(FUELTYPE, ROSeq, HR, CFB) {
 
   #Eq. 72 (FCFDG 1992)
   #Calculate the alpha constant for the DISTt calculation
-  alpha <- ifelse(FUELTYPE %in% c("C1", "O1A", "O1B", "S1", "S2", "S3", "D1"),
-                  0.115,
-                  0.115 - 18.8 * (CFB**2.5) * exp(-8* CFB))
+  alpha <- .DistanceAtTimeFunctions[FUELTYPE][[1]](CFB)
   #Eq. 71 (FCFDG 1992) Calculate Head fire spread distance
   DISTt  <- ROSeq * (HR + exp(-alpha * HR) / alpha - 1 / alpha)
   
