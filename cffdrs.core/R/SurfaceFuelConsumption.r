75662725f3dbfaeb6e9414f430148d24da08e96b
Jordan Evens <jordan.evens@canada.ca>
2021-11-19 07:58:16 -0500

    changed to use lookup for SurfaceFuelConsumption instead of multiple ifs

--- cffdrs.core/R/SurfaceFuelConsumption.r
+++ cffdrs.core/R/SurfaceFuelConsumption.r
@@ -1,3 +1,76 @@
+.SurfaceFuelConsumptionFunctions <- (function()
+{
+  sfcC1 <- function(FFMC, BUI, PC, GFL)
+  {
+    #Eqs. 9a, 9b (Wotton et. al. 2009) - Solving the lower bound of FFMC value
+    # for the C1 fuel type SFC calculation
+    return (ifelse(FFMC > 84,
+                   0.75 + 0.75 * (1 - exp(-0.23 * (FFMC - 84)))**0.5,
+                   0.75 - 0.75 * (1 - exp(-0.23 * (84 - FFMC)))**0.5))
+  }
+  sfcC2M3M4 <- function(FFMC, BUI, PC, GFL)
+  {
+    #Eq. 10 (FCFDG 1992) - C2, M3, and M4 Fuel Types
+    return (5.0 * (1 - exp(-0.0115 * BUI)))
+  }
+  sfcC3C4 <- function(FFMC, BUI, PC, GFL)
+  {
+    #Eq. 11 (FCFDG 1992) - C3, C4 Fuel Types
+    return (5.0 * (1 - exp(-0.0164 * BUI))**2.24)
+  }
+  sfcC5C6 <- function(FFMC, BUI, PC, GFL)
+  {
+    #Eq. 12 (FCFDG 1992) - C5, C6 Fuel Types
+    return (5.0 * (1 - exp(-0.0149 * BUI))**2.48)
+  }
+  sfcC7 <- function(FFMC, BUI, PC, GFL)
+  {
+    #Eqs. 13, 14, 15 (FCFDG 1992) - C7 Fuel Types
+    return (ifelse(FFMC > 70,
+                   2 * (1 - exp(-0.104 * (FFMC - 70))),
+                   0) + 1.5 * (1 - exp(-0.0201 * BUI)))
+    
+  }
+  sfcD1 <- function(FFMC, BUI, PC, GFL)
+  {
+    #Eq. 16 (FCFDG 1992) - D1 Fuel Type
+    return (1.5 * (1 - exp(-0.0183 * BUI)))
+  }
+  sfcM1M2 <- function(FFMC, BUI, PC, GFL)
+  {
+    #Eq. 17 (FCFDG 1992) - M1 and M2 Fuel Types
+    return (PC / 100 * (5.0 * (1 - exp(-0.0115 * BUI))) + 
+              ((100 - PC) / 100 * (1.5 * (1 - exp(-0.0183 * BUI)))))
+  }
+  sfcO1 <- function(FFMC, BUI, PC, GFL)
+  {
+    #Eq. 18 (FCFDG 1992) - Grass Fuel Types
+    return (GFL)
+  }
+  sfcS1 <- function(FFMC, BUI, PC, GFL)
+  {
+    #Eq. 19, 20, 25 (FCFDG 1992) - S1 Fuel Type
+    return (4.0 * (1 - exp(-0.025 * BUI)) + 4.0 * (1 - exp(-0.034 * BUI)))
+  }
+  sfcS2 <- function(FFMC, BUI, PC, GFL)
+  {
+    #Eq. 21, 22, 25 (FCFDG 1992) - S2 Fuel Type
+    return (10.0 * (1 - exp(-0.013 * BUI)) + 6.0 * (1 - exp(-0.060 * BUI)))
+  }
+  sfcS3 <- function(FFMC, BUI, PC, GFL)
+  {
+    #Eq. 23, 24, 25 (FCFDG 1992) - S3 Fuel Type
+    return (12.0 * (1 - exp(-0.0166 * BUI)) + 20.0 * (1-exp(-0.0210 * BUI)))
+  }
+  result <- list(sfcC1, sfcC2M3M4, sfcC3C4, sfcC3C4, sfcC5C6, sfcC5C6, sfcC7,
+                 sfcD1, sfcM1M2, sfcM1M2, sfcC2M3M4, sfcC2M3M4, sfcO1, sfcO1,
+                 sfcS1, sfcS2, sfcS3)
+  names(result) <- c("C1", "C2", "C3", "C4", "C5", "C6", "C7",
+                     "D1", "M1", "M2", "M3", "M4", "O1A", "O1B",
+                     "S1", "S2", "S3")
+  return (result)
+})()
+
 #' Surface Fuel Consumption Calculator
 #' 
 #'   Computes the Surface Fuel Consumption by Fuel Type.
@@ -23,53 +96,9 @@
 #' 
 #' @export SurfaceFuelConsumption
 SurfaceFuelConsumption <- function(FUELTYPE, FFMC, BUI, PC, GFL) {
-
   SFC <- rep(-999,length(FFMC))
-  #Eqs. 9a, 9b (Wotton et. al. 2009) - Solving the lower bound of FFMC value
-  # for the C1 fuel type SFC calculation
-  SFC <- ifelse(FUELTYPE=="C1", 
-          ifelse(FFMC > 84, 
-            0.75 + 0.75 * (1 - exp(-0.23 * (FFMC - 84)))**0.5,
-            0.75 - 0.75 * (1 - exp(-0.23 * (84 - FFMC)))**0.5),
-          SFC)
-  #Eq. 10 (FCFDG 1992) - C2, M3, and M4 Fuel Types
-  SFC <- ifelse(FUELTYPE == "C2" | FUELTYPE == "M3" | FUELTYPE == "M4", 
-          5.0 * (1 - exp(-0.0115 * BUI)),
-          SFC)
-  #Eq. 11 (FCFDG 1992) - C3, C4 Fuel Types
-  SFC <- ifelse(FUELTYPE == "C3" | FUELTYPE == "C4", 
-          5.0 * (1 - exp(-0.0164 * BUI))**2.24,
-          SFC)
-  #Eq. 12 (FCFDG 1992) - C5, C6 Fuel Types
-  SFC <- ifelse(FUELTYPE == "C5" | FUELTYPE == "C6",
-          5.0 * (1 - exp(-0.0149 * BUI))**2.48,
-          SFC)
-  #Eqs. 13, 14, 15 (FCFDG 1992) - C7 Fuel Types
-  SFC <- ifelse(FUELTYPE == "C7", 
-          ifelse(FFMC > 70, 2 * (1 - exp(-0.104 * (FFMC - 70))), 
-            0) + 1.5 * (1 - exp(-0.0201 * BUI)),
-          SFC)
-  #Eq. 16 (FCFDG 1992) - D1 Fuel Type
-  SFC <- ifelse(FUELTYPE == "D1", 1.5 * (1 - exp(-0.0183 * BUI)), SFC)
-  #Eq. 17 (FCFDG 1992) - M1 and M2 Fuel Types
-  SFC <- ifelse(FUELTYPE == "M1" | FUELTYPE == "M2", 
-          PC / 100 * (5.0 * (1 - exp(-0.0115 * BUI))) + 
-           ((100 - PC) / 100 * (1.5 * (1 - exp(-0.0183 * BUI)))), 
-          SFC)
-  #Eq. 18 (FCFDG 1992) - Grass Fuel Types
-  SFC <- ifelse(FUELTYPE == "O1A" | FUELTYPE == "O1B", GFL, SFC)
-  #Eq. 19, 20, 25 (FCFDG 1992) - S1 Fuel Type
-  SFC <- ifelse(FUELTYPE == "S1", 
-          4.0 * (1 - exp(-0.025 * BUI)) + 4.0 * (1 - exp(-0.034 * BUI)), 
-          SFC)
-  #Eq. 21, 22, 25 (FCFDG 1992) - S2 Fuel Type
-  SFC <- ifelse(FUELTYPE == "S2", 
-          10.0 * (1 - exp(-0.013 * BUI)) + 6.0 * (1 - exp(-0.060 * BUI)), 
-          SFC)
-  #Eq. 23, 24, 25 (FCFDG 1992) - S3 Fuel Type
-  SFC <- ifelse(FUELTYPE == "S3", 
-          12.0 * (1 - exp(-0.0166 * BUI)) + 20.0 * (1-exp(-0.0210 * BUI)),
-          SFC)
+  # NOTE: need to index the result because it's still a list
+  SFC <- .SurfaceFuelConsumptionFunctions[FUELTYPE][[1]](FFMC, BUI, PC, GFL)
   #Constrain SFC value
   SFC <- ifelse(SFC <= 0, 0.000001, SFC)
   
