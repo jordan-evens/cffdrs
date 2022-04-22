89f39a48ba1cf659039fbdbf83530a8536457243
Jordan Evens <jordan.evens@canada.ca>
2022-04-22 16:16:32 -0400

    added original fbpRaster() outputs to test data

--- makeTestData.r
+++ makeTestData.r
@@ -1531,3 +1531,43 @@ saveResults('SimardRateOfSpreadLine',
 data("test_pros", package="cffdrs")
 saveResults('SimardRateOfSpreadPoint',
             cffdrs::pros(test_pros))
+###########################################################################
+# Raster tests
+###########################################################################
+saveRasters <- function(name, rasters)
+{
+  out_dir <- paste0(PATH, "/rasters/", name, "/")
+  dir.create(out_dir, showWarnings=FALSE, recursive=TRUE)
+  for(i in 1:length(names(rasters)))
+  {
+    n <- names(rasters)[[i]]
+    lyr <- rasters[[n]]
+    write.csv(as.data.frame(lyr), paste0(out_dir, n, ".csv"), row.names=FALSE)
+  }
+}
+
+test_fbpRaster <- stack(system.file("extdata", "test_fbpRaster.tif", package="cffdrs"))
+input<-test_fbpRaster
+# Stack doesn't hold the raster layer names, we have to assign
+# them:
+names(input)<-c("FuelType","LAT","LONG","ELV","FFMC","BUI", "WS","WD","GS","Dj","D0","hr","PC",
+                "PDF","GFL","cc","theta","Accel","Aspect","BUIEff","CBH","CFL","ISI")
+# Primary outputs:
+system.time(foo1<-fbpRaster(input = input))
+saveRasters("foo1", foo1)
+# Using the "select" option:
+system.time(foo2<-fbpRaster(input = input,select=c("HFI","TFC", "ROS")))
+saveRasters("foo2", foo2)
+# Secondary outputs:
+system.time(foo3<-fbpRaster(input = input,output="S"))
+saveRasters("foo3", foo3)
+# All outputs:
+system.time(foo4<-fbpRaster(input = input,output="A"))
+saveRasters("foo4", foo4)
+### Additional, longer running examples  ###
+# Keep only the required input layers, the other layers would be
+# assigned with default values:
+# keep only the required inputs:
+dat0<-input[[c("FuelType","LAT","LONG","FFMC","BUI","WS","GS", "Dj","Aspect")]]
+system.time(foo5<-fbpRaster(input = dat0,output="A"))
+saveRasters("foo5", foo5)
